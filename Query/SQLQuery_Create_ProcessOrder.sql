13550ef14dd12ac406843ac3b91cd31fe893bd1f
Fr0thgar <jona270r@edu.easj.dk>
2019-05-01 09:44:20 +0200

    Update SQLQuery_Create_ProcessOrder.sql

--- Query/SQLQuery_Create_ProcessOrder.sql
+++ Query/SQLQuery_Create_ProcessOrder.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE ProcessOrder(
 	ProcessOrderNo INT NOT NULL PRIMARY KEY,
-	DateTime DATETIME NOT NULL,
+	DateTime DATETIME NOT NULL DEFAULT(getdate()),
 	ProductNo INT FOREIGN KEY REFERENCES Product(ProductNo),
 	EmployeeNo INT FOREIGN KEY REFERENCES Employee(EmployeeNo),
 	BatchCode VARCHAR FOREIGN KEY REFERENCES Batch(BatchCode) 
