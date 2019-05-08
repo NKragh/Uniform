6f183bd17769700599c4887b1cb4d3fddba755f9
NKragh <39466838+NKragh@users.noreply.github.com>
2019-05-08 16:04:37 +0200

    Klassisk
    Nikolaj at skrive i masteren

--- UniformApp/ViewModel/ProcessOrderViewModel.cs
+++ UniformApp/ViewModel/ProcessOrderViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
+using UniformApp.Annotations;
 using UniformApp.Handler;
+using UniformApp.Model;
 
 namespace UniformApp.ViewModel
 {
@@ -17,34 +21,44 @@ namespace UniformApp.ViewModel
         public ICommand CreateProcessOrderCommand { get; set; }
         public ICommand DeleteProcessOrderCommand { get; set; }
         public ICommand EditProcessOrderCommand { get; set; }
-        public ICommand SaveProcessOrderCommand { get; set; }
+        public ICommand ReadProcessOrderCommand { get; set; }
 
         private ProcessOrder _newProcessOrder;
+        private string path;
+        public ObservableCollection<ProcessOrder> processOrders = new ObservableCollection<ProcessOrder>();
 
         public ProcessOrder NewProcessOrder
         {
-            get { return new ProcessOrder;}
+            get { return new ProcessOrder(); }
             set
             {
                 _newProcessOrder = value;
                 OnPropertyChanged();
             }
         }
-
+        
         public ProcessOrderViewModel()
         {
             ProcessOrderHandler poHandler = new ProcessOrderHandler(this);
+            path = "http://localhost:55478/";
+            //CreateProcessOrderCommand = new RelayCommand (poHandler.CreateProcessOrderAsync);
+            ReadProcessOrderCommand = new RelayCommand(poHandler.ReadProcessOrder);
+            DeleteProcessOrderCommand = new RelayCommand(poHandler.DeleteProcessOrder);
+            EditProcessOrderCommand = new RelayCommand(poHandler.UpdateProcessOrder);
 
-            CreateProcessOrderCommand = new RelayCommand (ProcessOrderHandler.CreateProcessOrder);
-            DeleteProcessOrderCommand = new RelayCommand(ProcessOrderHandler.DeleteProcessOrder);
-            EditProcessOrderCommand = new RelayCommand(ProcessOrderHandler.EditProcessOrder);
-
-            ProcessOrder = NewProcessOrder();
-
+            //ProcessOrder = NewProcessOrder();
+            processOrders.Add(new ProcessOrder(123, DateTime.Now, "Hejsa", true, 12,123456, 654321));
 
             //relaycommands
         }
 
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
