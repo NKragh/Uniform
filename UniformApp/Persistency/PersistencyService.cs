c8a5f369088719d5b7479463f04380676e81f9eb
J Nielsen <buhros@hotmail.com>
2019-05-09 16:29:34 +0200

    Vigtigste commit til dato 2.0

--- UniformApp/Persistency/PersistencyService.cs
+++ UniformApp/Persistency/PersistencyService.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using UniformApp.Model;
 using UniformApp.ViewModel;
+using Newtonsoft.Json;
 
 namespace UniformApp.Persistency
 {
     class PersistencyService
     {
-        public static async Task<List<T>> ReadProcessOrder<T>()
+        public static async Task<List<ProcessOrder>> ReadProcessOrder<T>()
         {
             string Path = "http://localhost:55478";
-            List<T> targetList = new List<T>();
+            List<ProcessOrder> targetList = new List<ProcessOrder>();
 
             HttpClientHandler handler = new HttpClientHandler();
             handler.UseDefaultCredentials = true; //possible not working
+
             using (var client = new HttpClient(handler))
             {
                 client.BaseAddress = new Uri(Path);
@@ -31,11 +36,48 @@ namespace UniformApp.Persistency
                     //await rTask;
                     if (response.IsSuccessStatusCode)
                     {
-                        IEnumerable<T> procesorderData = response.Content.ReadAsAsync<IEnumerable<T>>().Result;
-                        foreach (var p in procesorderData)
-                        {
-                            targetList.Add(p);
-                        }
+
+                        string res = await response.Content.ReadAsStringAsync();
+                        var content = JsonConvert.DeserializeObject(res);
+
+
+
+
+                        //var o = response.Content.ReadAsAsync<ProcessOrder>(new[] {new JsonMediaTypeFormatter()});
+
+
+
+                        //var fuckoff = response.Content.ReadAsStreamAsync().Result;
+                        //Encoding encode =
+                        //    System.Text.Encoding.GetEncoding("utf-8");
+
+                        //StreamReader sr = new StreamReader(fuckoff, encode);
+                        //Char[] read = new Char[1024];
+
+                        //int Count = sr.Read(read, 0, 1024);
+                        //List<ProcessOrder> processOrders = new List<ProcessOrder>();
+
+
+
+
+
+
+
+                        //IEnumerable<ProcessOrder> procesorderData = response.Content.ReadAsAsync<IEnumerable<ProcessOrder>>().Result;
+                        //foreach (var p in procesorderData)
+                        //{
+                        //    ProcessOrder po = new ProcessOrder(Convert.ToInt32(p.Attribute));
+                        //}
+
+
+                        //ProcessOrder blah = response.Content.ReadAsJsonAsync<ProcessOrder>().Result;
+                        //Console.WriteLine(blah);
+                        //string blah2 = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(blah).ToString(); 
+                        //ProcessOrder procesorderData =  response.Content.ReadAsAsync<ProcessOrder>().Result;
+                        //foreach (ProcessOrder p in procesorderData)
+                        //{
+                        //targetList.Add(procesorderData);
+                        //}
                     }
                 }
                 catch (Exception e)
@@ -49,4 +91,14 @@ namespace UniformApp.Persistency
             }
         }
     }
+
+    public static class HTTPClientExtensions
+    {
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
+        {
+            var dataAsString = await content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(dataAsString);
+        }
+    }
+
 }
