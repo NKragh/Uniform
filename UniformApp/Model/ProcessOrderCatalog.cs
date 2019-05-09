7dde161de961456c5b0e5a01de851e32a6ee8e63
NKragh <39466838+NKragh@users.noreply.github.com>
2019-05-09 12:33:13 +0200

    iyhoi

--- UniformApp/Model/ProcessOrderCatalog.cs
+++ UniformApp/Model/ProcessOrderCatalog.cs
@@ -12,7 +12,11 @@ namespace UniformApp.Model
     class ProcessOrderCatalog
     {
         private static ProcessOrderCatalog _instance = new ProcessOrderCatalog();
-        public static ProcessOrderCatalog Instance { get => _instance; }
+
+        public static ProcessOrderCatalog Instance
+        {
+            get => _instance;
+        }
 
         public ObservableCollection<ProcessOrder> ProcessOrderList { get; set; }
 
@@ -36,6 +40,7 @@ namespace UniformApp.Model
             else
             {
                 ProcessOrderList.Add(new ProcessOrder(1,DateTime.Now,"1",true,1,1,1));
+                ProcessOrderList.Add(new ProcessOrder(2, DateTime.Now, "2", true, 2, 2, 2));
             }
         }
     }
