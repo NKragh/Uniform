d9f8e75e21eeff07759079399537fb46858f75bd
J Nielsen <buhros@hotmail.com>
2019-05-10 10:38:33 +0200

    Fixed.

--- UniformApp/View/ProcessOrderPage.xaml.cs
+++ UniformApp/View/ProcessOrderPage.xaml.cs
@@ -20,9 +20,9 @@ namespace UniformApp.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ProcessOrder : Page
+    public sealed partial class ProcessOrderPage : Page
     {
-        public ProcessOrder()
+        public ProcessOrderPage()
         {
             this.InitializeComponent();
         }
