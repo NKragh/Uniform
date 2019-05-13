84df9166c459259e52bc9c38548198a59ae428a2
jonasras1994 <jona72c5@edu.easj.dk>
2019-05-13 13:41:05 +0200

    WeightCheck.cs

--- UniformApp/Model/WeightCheck.cs
+++ UniformApp/Model/WeightCheck.cs
@@ -8,5 +8,22 @@ namespace UniformApp.Model
 {
     class WeightCheck
     {
+        private int _processNo;
+        private DateTime _checkTime;
+        private float _weight1;
+        private float _weight2;
+        private float _weight3;
+        private float _weight4;
+        private float _weight5;
+        private float _weight6;
+        private string _comments;
+        private int _employeeNo;
+        private int _productNo;
+
+        public DateTime CheckTime
+        {
+            get { return DateTime.Now; }
+            set { _checkTime = value; }
+        }
     }
 }
