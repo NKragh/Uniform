1e6815dc5eb2ef96b09174b61371cbf76bd08d50
Fr0thgar <jona270r@edu.easj.dk>
2019-05-15 10:28:49 +0200

    created querys

---
+++ Query/SQLQuery_Create_ShiftCheck.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE ShiftCheck(
+	ProcessOrderNo INT NOT NULL FOREIGN KEY REFERENCES ProcessOrder(ProcessOrderNo),
+	CheckTime TIME NOT NULL DEFAULT (CONVERT (time, GETDATE())),
+	TopLabel BIT NOT NULL,
+	TapPipe BIT NOT NULL, 
+	Sugar BIT NOT NULL,
+	PalletNo INT NOT NULL,
+	EmployeeNo INT NOT NULL FOREIGN KEY REFERENCES Employee(EmployeeNo)
+	PRIMARY KEY (ProcessOrderNo, CheckTime)
+	);
---
+++ Query/SQLQuery_Create_WeightCheck.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE WeightCheck(
+	ProcessOrderNo INT NOT NULL FOREIGN KEY REFERENCES ProcessOrder(ProcessOrderNo),
+	CheckTime TIME NOT NULL DEFAULT (CONVERT (time, GETDATE())),
+	Weight1 FLOAT NOT NULL,
+	Weight2 FLOAT NOT NULL,
+	Weight3 FLOAT NOT NULL,
+	Weight4 FLOAT NOT NULL,
+	Weight5 FLOAT NOT NULL,
+	Weight6 FLOAT NOT NULL,
+	Droptest BIT NOT NULL,
+	Comment NVARCHAR(140) NOT NULL,
+	EmployeeNo INT FOREIGN KEY REFERENCES Employee(EmployeeNo),
+	ProductNo INT FOREIGN KEY REFERENCES Product(ProductNo),
+	PRIMARY KEY (ProcessOrderNo, CheckTime)
+	);
