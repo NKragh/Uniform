1d0b55a6f6f08d10489fcb001be2eba42061af3c
Fr0thgar <jona270r@edu.easj.dk>
2019-05-16 09:01:39 +0200

    Stuff
    ShiftCheckHandler
    and ShiftCheckviewmodel

---
+++ UniformApp/Handler/ShiftCheckHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniformApp.ViewModel;
+
+namespace UniformApp.Handler
+{
+    class ShiftCheckHandler
+    {
+        public ShiftCheckViewModel ShiftCheckViewModel { get; set; }
+
+        public ShiftCheckHandler(ShiftCheckViewModel viewModel)
+        {
+            ShiftCheckViewModel = viewModel;
+        }
+
+        // <summary>
+        // Default C-R-U-D features for application
+        // </summary>
+
+        public void CreateShiftCheck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadShiftCheck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateShiftCheck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteShiftCheck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
