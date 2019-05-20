3829c5eb432ee692347c6f291547dcf6517983cc
Fr0thgar <jona270r@edu.easj.dk>
2019-05-20 12:33:35 +0200

    SampleCheck.cs

---
+++ UniformApp/Model/SampleCheck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniformApp.Model
+{
+    class SampleCheck
+    {
+        private int _processOrderNo { get; set; }
+        private TimeSpan _checkTime { get; set; }
+        private int _employeeNo { get; set; }
+
+        public int ProcessOrderNo
+        {
+            get => _processOrderNo;
+            set => _processOrderNo = value;
+        }
+
+        public TimeSpan CheckTime { get => _checkTime; set => _checkTime = value; }
+
+        public int EmployeeNo
+        {
+            get => _employeeNo;
+            set => _employeeNo = value;
+        }
+
+
+        public SampleCheck(int processOrderNo, TimeSpan checkTime, bool sampleCheck, int employeeNo)
+        {
+            ProcessOrderNo = processOrderNo;
+            CheckTime = checkTime;
+            EmployeeNo = employeeNo;
+        }
+
+        public SampleCheck()
+        {
+            
+        }
+
+    }
+}
